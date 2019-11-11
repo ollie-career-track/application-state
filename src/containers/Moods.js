94e1a1086a04833e4810d579712a1251ccd0ed5c
olliecomet <leyechi@gmail.com>
2019-11-11 13:50:01 -0800

    moved selectors to selectors folder

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -2,22 +2,7 @@ import React, { Component } from 'react';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
 import actions from '../actions/actions';
-
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
-
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
-
-  return '😀';
-};
+import getFace from '../selectors/selectors';
 
 export default class Moods extends Component {
   render() {
