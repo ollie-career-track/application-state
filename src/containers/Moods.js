185d1e245716c8d43e8be1badd813ec04cbf81c2
olliecomet <leyechi@gmail.com>
2019-11-11 14:10:44 -0800

    dispatch and mood container

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
 import actions from '../actions/actions';
 import getFace from '../selectors/selectors';
 
-export default class Moods extends Component {
+class Moods extends Component {
   render() {
     const face = getFace(this.state);
     const controlActions = actions.map(action => ({
@@ -20,3 +21,40 @@ export default class Moods extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  coffees: state.coffees,
+  snacks: state.snacks,
+  naps: state.naps,
+  studies: state.studies
+});
+
+const mapDispatchToProps = dispatch => ({
+  drinkCoffee() {
+    dispatch({
+      type: 'DRINK_COFFEE'
+    });
+  },
+  eatSnack() {
+    dispatch({
+      type: 'EAT_SNACK'
+    });
+  },
+  takeNap() {
+    dispatch({
+      type: 'TAKE_NAP'
+    });
+  },
+  study() {
+    dispatch({
+      type: 'STUDY'
+    }); 
+  }
+});
+
+const MoodContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Moods);
+
+export default MoodContainer;
--- src/store.js
+++ src/store.js
@@ -13,7 +13,7 @@ function reducer(state = initialState, action) {
       return { ...state, coffees: state.coffees + 1 };
     case 'EAT_SNACK':
       return { ...state, snacks: state.snacks + 1 };
-    case 'TAKE_NAPS':
+    case 'TAKE_NAP':
       return { ...state, naps: state.naps + 1 };
     case 'STUDY':
       return { ...state, studies: state.studies + 1 };
