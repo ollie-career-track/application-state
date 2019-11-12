b54ddd56359e5b14e1b4123503be526ecf1cb3b7
olliecomet <leyechi@gmail.com>
2019-11-12 14:05:59 -0800

    mood action tests

---
+++ src/actions/moodActions.test.js
@@ -0,0 +1,32 @@
+import { 
+  DRINK_COFFEE, drinkCoffee, 
+  EAT_SNACKS, eatSnacks,
+  TAKE_NAP, takeNap,
+  STUDY, study
+} from './moodActions';
+
+describe('mood actions', () => {
+  it('creates a drink coffee action', () => {
+    expect(drinkCoffee()).toEqual({
+      type: DRINK_COFFEE
+    });
+  });
+
+  it('creates an eat snacks action', () => {
+    expect(eatSnacks()).toEqual({
+      type: EAT_SNACKS
+    });
+  });
+
+  it('creates a take nap action', () => {
+    expect(takeNap()).toEqual({
+      type: TAKE_NAP
+    });
+  });
+
+  it('creates a study action', () => {
+    expect(study()).toEqual({
+      type: STUDY
+    });
+  });
+});
