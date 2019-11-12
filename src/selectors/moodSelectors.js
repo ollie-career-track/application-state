a67b19e7a97ef5990e5afc7f1025eab8cfa8ba16
olliecomet <leyechi@gmail.com>
2019-11-12 14:14:02 -0800

    mood selectors

---
+++ src/selectors/moodSelectors.js
@@ -0,0 +1,4 @@
+export const getCoffees = state => state.coffees;
+export const getSnacks = state => state.snacks;
+export const getNaps = state => state.naps;
+export const getStudies = state => state.studies;
